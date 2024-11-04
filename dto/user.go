6bf1c866cd3ab1ad156986640481c211c0eaa1df
Rama Diaz <ramadiaz221@gmail.com>
2024-11-04 12:32:24 +0700

    UPDATE of dto.User

--- dto/user.go
+++ dto/user.go
@@ -1,10 +1,10 @@
 package dto
 
 type User struct {
-	ID          string `json:"id"`
-	Email       string `json:"email" form:"email" binding:"required"`
-	Password    string `json:"password,omitempty" form:"password"`
-	FirstName   string `json:"first_name" form:"first_name" binding:"required"`
-	LastName    string `json:"last_name" form:"last_name" binding:"required"`
-	GoogleToken string `json:"google_token,omitempty" form:"google_token"`
+	ID        string `json:"id"`
+	Email     string `json:"email" form:"email" binding:"required"`
+	Password  string `json:"password,omitempty" form:"password"`
+	FirstName string `json:"first_name" form:"first_name" binding:"required"`
+	LastName  string `json:"last_name" form:"last_name" binding:"required"`
+	GoogleUID string `json:"google_uid,omitempty" form:"google_uid"`
 }
