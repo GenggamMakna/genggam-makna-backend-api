342c9262ce972a8131c75797eb3adae941ba5eb7
Rama Diaz <ramadiaz221@gmail.com>
2024-12-03 14:51:15 +0700

    INIT of gcloud bucket usage

--- services/storage_svc.go
+++ services/storage_svc.go
@@ -1,28 +1,35 @@
 package services
 
 import (
+	"context"
 	"crypto/rand"
 	"encoding/hex"
-	"os"
-	"path/filepath"
+	"fmt"
+
+	"cloud.google.com/go/storage"
 )
 
 func (s *compServices) StoreImage(image []byte) (string, error) {
 	randomBytes := make([]byte, 16)
 	if _, err := rand.Read(randomBytes); err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to generate random name: %w", err)
 	}
 	randomName := hex.EncodeToString(randomBytes) + ".png"
 
-	publicFolderPath := "./public"
-	if err := os.MkdirAll(publicFolderPath, os.ModePerm); err != nil {
-		return "", err
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return "", fmt.Errorf("failed to create storage client: %w", err)
 	}
+	defer client.Close()
 
-	filePath := filepath.Join(publicFolderPath, randomName)
+	bucket := client.Bucket("xanny-bucket")
 
-	if err := os.WriteFile(filePath, image, 0644); err != nil {
-		return "", err
+	writer := bucket.Object(randomName).NewWriter(ctx)
+	defer writer.Close()
+
+	if _, err := writer.Write(image); err != nil {
+		return "", fmt.Errorf("failed to write image to bucket: %w", err)
 	}
 
 	return randomName, nil
