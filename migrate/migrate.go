39ad55176f55216567a32025c5917ad860923b69
Rama Diaz <ramadiaz221@gmail.com>
2024-12-11 10:23:23 +0700

    UPDATE of migrate.go

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	db := config.InitDB()
 
+	db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
+
 	err := db.AutoMigrate(&models.Client{}, &models.Users{})
 	if err != nil {
 		panic("failed to migrate database: " + err.Error())
